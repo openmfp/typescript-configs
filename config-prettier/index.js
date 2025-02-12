0f9c796efc8cb5a99456b4aff99c86b9951e970c
robertIsaac <robert.isaac@live.com>
2025-02-12 07:51:41 +0200

    feat: update prettier config (#50)
    - add `@trivago/prettier-plugin-sort-imports` as peer dep
    - bump minimum prettier version to 3.5
    - change `trailingComma` to all as it minimize the chance of false conflict
    - move from cmj to esm

--- config-prettier/index.js
+++ config-prettier/index.js
@@ -1,10 +1,16 @@
 // See also https://prettier.io/docs/en/options.html
 
-module.exports = {
+const config = {
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   singleQuote: true,
   semi: true,
-  trailingComma: "es5"
+  trailingComma: "all",
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  // for @trivago/prettier-plugin-sort-imports
+  importOrderParserPlugins: ["typescript", "decorators"],
+  "importOrderSortSpecifiers": true
 };
+
+export default config;
