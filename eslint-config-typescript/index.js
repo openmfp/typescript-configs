33c5ed97244ff594f843b51171e9a6cc944ea19e
Robert Isaac <robert.isaac@sap.com>
2025-02-18 13:24:44 +0200

    chore: format files

--- eslint-config-typescript/index.js
+++ eslint-config-typescript/index.js
@@ -5,35 +5,35 @@ import pluginJest from "eslint-plugin-jest";
 import pluginPrettier from "eslint-config-prettier";
 
 export default pluginTs.config(
-    // config to all javascript and typescript files
-    {
-        files: ["**/*.ts", "**/*.js"],
-        extends: [pluginJs.configs.recommended, pluginPrettier],
-        rules: {
-            "no-duplicate-imports": "error",
-        }
+  // config to all javascript and typescript files
+  {
+    files: ["**/*.ts", "**/*.js"],
+    extends: [pluginJs.configs.recommended, pluginPrettier],
+    rules: {
+      "no-duplicate-imports": "error",
     },
-    {
-        // config to all typescript files only
-        files: ["**/*.ts"],
-        extends: [pluginTs.configs.recommended, pluginTs.configs.stylistic],
-        rules: {
-            "@typescript-eslint/no-unused-vars": [
-                "error",
-                {
-                    argsIgnorePattern: '^_',
-                    caughtErrorsIgnorePattern: '^_',
-                    ignoreRestSiblings: true,
-                },
-            ],
-        }
+  },
+  {
+    // config to all typescript files only
+    files: ["**/*.ts"],
+    extends: [pluginTs.configs.recommended, pluginTs.configs.stylistic],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
     },
-    {
-      // config to test files only
-      files: ['**/*.spec.ts'],
-      ...pluginJest.configs['flat/recommended'],
-      rules: {
-        ...pluginJest.configs['flat/recommended'].rules,
-      }
+  },
+  {
+    // config to test files only
+    files: ["**/*.spec.ts"],
+    ...pluginJest.configs["flat/recommended"],
+    rules: {
+      ...pluginJest.configs["flat/recommended"].rules,
     },
+  },
 );
