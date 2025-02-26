2f3c3c285e432c54b7e9b91247afcb33870f69ae
robertIsaac <robert.isaac@live.com>
2025-02-26 11:00:50 +0200

    feat(STARGATE-219): update typescript config (#55)
    - switch to eslint flat config
    - migrate from old @typescript-eslint/* to the new typescript-eslint
    - migrate from old @angular-eslint/* to the new angular-eslint
    - bump minimum eslint version to 9.20
    - add extensions to @typescript-eslint/no-unused-vars starting with _
    - move from cmj to esm
    - add index.d.ts file

--- eslint-config-typescript/index.js
+++ eslint-config-typescript/index.js
@@ -1,28 +1,39 @@
-module.exports = {
-  root: true,
-  overrides: [
-    {
-      files: ["*.ts"],
-      parserOptions: {
-        project: ["tsconfig.json"],
-        createDefaultProgram: true,
-      },
-      extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:prettier/recommended",
-        "prettier",
+// @ts-check
+import pluginTs from "typescript-eslint";
+import pluginJs from "@eslint/js";
+import pluginJest from "eslint-plugin-jest";
+import pluginPrettier from "eslint-config-prettier";
+
+export default pluginTs.config(
+  // config to all javascript and typescript files
+  {
+    files: ["**/*.ts", "**/*.js"],
+    extends: [pluginJs.configs.recommended, pluginPrettier],
+    rules: {
+      "no-duplicate-imports": "error",
+    },
+  },
+  {
+    // config to all typescript files only
+    files: ["**/*.ts"],
+    extends: [pluginTs.configs.recommended, pluginTs.configs.stylistic],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
       ],
     },
-    {
-      files: ["*.spec.ts"],
-      plugins: ["jest"],
-      rules: {
-        // See also https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/unbound-method.md
-        "@typescript-eslint/unbound-method": "off",
-        "jest/unbound-method": "error",
-      },
+  },
+  {
+    // config to test files only
+    files: ["**/*.spec.ts"],
+    ...pluginJest.configs["flat/recommended"],
+    rules: {
+      ...pluginJest.configs["flat/recommended"].rules,
     },
-  ],
-};
+  },
+);
